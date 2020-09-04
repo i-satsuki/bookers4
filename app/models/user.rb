389d9b0541a67cba9b40724c7e781f0e79704e77
ishida <ishida@ishidanoMacBook-Air.local>
2020-09-04 18:16:22 +0900

    [Finish]bookers4

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,23 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   attachment :profile_image, destroy: false
 
+  has_many :following_relationships, foreign_key: "follower_id", class_name: "Relationship", dependent: :destroy
+  has_many :followings, through: :following_relationships
+  has_many :follower_relationships, foreign_key: "following_id", class_name: "Relationship", dependent: :destroy
+  has_many :followers, through: :follower_relationships
+
+	  def following?(other_user)
+	    following_relationships.find_by(following_id: other_user.id)
+	  end
+
+	  def follow!(other_user)
+	    following_relationships.create!(following_id: other_user.id)
+	  end
+
+	  def unfollow!(other_user)
+	    following_relationships.find_by(following_id: other_user.id).destroy
+	  end
+
   #バリデーションは該当するモデルに設定する。エラーにする条件を設定できる。
   validates :name, presence: true, length: {maximum: 20, minimum: 2}
   validates :introduction, length: {maximum: 50}
